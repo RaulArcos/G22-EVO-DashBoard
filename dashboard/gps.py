2ebf695dc5173b39d48ebc62bb7ec07a55fb0ab0
Raul Arcos Herrera <48330837+RaulArcos@users.noreply.github.com>
2023-07-17 23:09:52 +0200

    Update gps.py

--- dashboard/gps.py
+++ dashboard/gps.py
@@ -9,15 +9,19 @@ class GPS:
 
     def receive_gps_data(self):
         while True:
-            ser = serial.Serial(self.port, baudrate=9200, timeout=1)
-            data = ser.readline().decode('utf-8')
+            try:
+                ser = serial.Serial(self.port, baudrate=9200, timeout=1)
+                data = ser.readline().decode('utf-8')
+            except serial.serialutil.SerialException as e:
+                print("GPS::receive_gps_data - No se reciven datos - Error: ", e)
             if data.startswith('$GPRMC'):
                 try:
                     msg = pynmea2.parse(data)
                     timestamp = int(time.perf_counter() * 1000)
-                    speed = msg.spd_over_grnd * 1.852
-                    self.excel_manager.save_gps_data(msg.latitude, msg.longitude, timestamp, speed)
-                    print("Tiempo", timestamp, "ms", "Latitud ", msg.latitude, " Longitud ", msg.longitude,
-                          " Speed ", speed, " km/h")
+                    if msg.spd_over_grnd is not None:
+                        speed = msg.spd_over_grnd * 1.852
+                        self.excel_manager.save_gps_data(msg.latitude, msg.longitude, timestamp, speed)
+                        print("Tiempo", timestamp, "ms", "Latitud ", msg.latitude, " Longitud ", msg.longitude,
+                            " Speed ", speed, " km/h")
                 except pynmea2.ParseError:
                     print("Error convirtiendo el serial a texto.")
